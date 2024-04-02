08912e75b2c9a8e8ed9a486d2520d44ba8e4069c
Viacheslav Melnyk <altt1m.zaskar@gmail.com>
2024-04-02 18:18:19 +0300

    Final

--- Source.cpp
+++ Source.cpp
@@ -1,205 +1,22 @@
 #include <iostream>;
+//#include "Task1.h";
+#include "Task2.h";
 
 using namespace std;
 
-struct Node
-{
-    int data;
-    Node* next;
-    Node* prev;
-};
-
-Node* InitList(int x)
-{
-    Node* c = new Node;
-    c->next = NULL;
-    c->prev = NULL;
-    c->data = x;
-    return c;
-}
-
-void AddNodeAfter(Node*& ptr, int x)
-{
-    if (ptr == NULL) return;
-
-    Node* c = new Node;
-    c->data = x;
-    c->next = ptr->next;
-    c->prev = ptr;
-    if (ptr->next != NULL)
-        ptr->next->prev = c;
-    ptr->next = c;
-    ptr = c;
-}
-
-void InsertBeg(Node*& head, int x)
-{
-    if (head == NULL) return;
-    Node* c = new Node;
-    c->data = x;
-    c->prev = head->prev;
-    c->next = head;
-    head->prev = c;
-    head = c;
-}
-
-Node* DeleteNode(Node* ptr)
-{
-    if (ptr == NULL) return 0;
-    Node* q = ptr->prev;
-    if (ptr->next != NULL) // not end
-    {
-        ptr->next->prev = q;
-        if (q) // not head
-        {
-            q->next = ptr->next;
-            delete ptr;
-            return q->next;
-        }
-        else // head
-        {
-            q = ptr->next;
-            delete ptr;
-            return q;
-        }
-    }
-    else // end
-    {
-        if (q) // not head
-        {
-            q->next = NULL;
-            delete ptr;
-            return q;
-
-        }
-        else // and head
-        {
-            delete ptr;
-            return NULL;
-        }
-    }
-}
-
-void Print_Right(Node* head)
-{
-    cout << "\nThe list from head to end:" << endl;
-    if (head == NULL)
-    {
-        cout << "Empty" << endl;
-        return;
-    }
-    Node* c = head;
-    while (c != NULL)
-    {
-        cout << c->data << " ";
-        c = c->next;
-    }
-    cout << endl;
-}
-
-void Print_Left(Node* end)
-{
-    cout << "\nThe list from end to head:" << endl;
-    if (end == NULL)
-    {
-        cout << "Empty" << endl;
-        return;
-    }
-    Node* c = end;
-    while (c != NULL)
-    {
-        cout << c->data << " ";
-        c = c->prev;
-    }
-    cout << endl;
-}
-
-void Free_Memory(Node*& head)
-{
-    if (head == NULL) return;
-    Node* c = head;
-    while (c != NULL)
-    {
-        head = head->next;
-        delete c;
-        c = head;
-    }
-}
-
-void Task1()
+int main()
 {
     // 15 | Створити лінійний двозв'язний список з цілих чисел.
     //      Вставити в список число 25 перед кожним елементом з додатним значенням.
     //      Вилучити зі списку всі від’ємні числа
 
-    Node* head = NULL, * end = NULL, * c;
-    int x;
-    cout << "Input 5 elements:" << endl;
-    for (int i = 1; i <= 5; i++)
-    {
-        cin >> x;
-        if (head == NULL)
-            head = end = InitList(x);
-        else
-            AddNodeAfter(end, x);
-    }
+    //Task1();
 
-    Print_Right(head);
-    Print_Left(end);
 
-    // Вставка числа 25 перед кожним елементом з додатним значенням
-    c = head;
-    while (c != NULL)
-    {
-        if (c == head && head->data > 0)
-        {
-            InsertBeg(head, 25); // Вставка перед head
-            c = c->next; // Повертаємося на те, на чому були
-        }
-        if (c->next != NULL && c->next->data > 0)
-        {
-            AddNodeAfter(c, 25);
-        }
-        c = c->next;
-    }
+    // 15 | Створити циклічний список цілих чисел.
+    //      З кожної пари сусідніх елементів вилучити менший за модулем
 
-    Print_Right(head);
-    Print_Left(end);
-
-    // Вилучення зі списку всіх від’ємних чисел
-    c = head;
-    while (c != NULL)
-    {
-        if (c->data < 0)
-        {
-            if (c == head)
-            {
-                head = c = DeleteNode(c);
-            }
-            else if (c == end)
-            {
-                end = c = DeleteNode(c);
-            }
-            else
-            {
-                c = DeleteNode(c);
-            }
-        }
-        else
-        {
-            c = c->next;
-        }
-
-    }
-
-    Print_Right(head);
-    Print_Left(end);
-}
-
-int main()
-{
-    Task1();
-
+    //Task2();
 
     return 0;
 }
---
+++ Task2.h
@@ -0,0 +1,143 @@
+#pragma once
+#include <iostream>;
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+};
+
+Node* InitList(double x)
+{
+    Node* c = new Node;
+    c->next = c;
+    c->data = x;
+    return c;
+}
+
+Node* AddNodeAfter(Node*& ptr, double x) // Getting next after added
+{
+    Node* c = new Node;
+    c->next = c;
+    c->data = x;
+    if (ptr == NULL) return NULL;
+    else
+    {
+        c->next = ptr->next;
+        ptr->next = c;
+        ptr = c;
+        return c->next;
+    }
+}
+
+void Print_List(Node* head)
+{
+    cout << "\nThe list:" << endl;
+    Node* c = head;
+    do
+    {
+        if (c)
+        {
+            cout << c->data << " ";
+            c = c->next;
+        }
+    } while (c != head);
+    cout << endl;
+}
+
+void Free_Memory(Node* head, Node* end)
+{
+    if (head == NULL) return;
+    Node* c = head;
+    end->next = NULL;
+    while (c != NULL)
+    {
+        head = head->next;
+        delete c;
+        c = head;
+    }
+}
+
+Node* DeleteNode(Node*& ptr, Node*& head, Node*& end) // Getting next after ptr
+{
+    if (ptr == NULL) return NULL;
+    if (ptr == head)
+    {
+        if (head == end)
+        {
+            delete ptr;
+            head = end = NULL;
+            return NULL;
+        }
+        else
+        {
+            head = head->next;
+            end->next = head;
+            delete ptr;
+            return head;
+        }
+    }
+
+    Node* prev = ptr->next;
+    while (prev && prev->next != ptr) // Looking for element before ptr by going through loop
+    {
+        prev = prev->next;
+    }
+
+    if (prev != ptr)
+    {
+        prev->next = ptr->next;
+        if (ptr == end) end = prev;
+        delete ptr;
+        return prev->next;
+    }
+    return NULL;
+}
+
+void Task2()
+{
+    // 15 | Створити циклічний список цілих чисел.
+    //      З кожної пари сусідніх елементів вилучити менший за модулем
+    Node* head = NULL, * end = NULL, * c;  double x;
+    cout << "Input 10 elements:" << endl;
+    for (int i = 1; i <= 10; i++)
+    {
+        cin >> x;
+        if (head == NULL)
+        {
+            head = end = InitList(x);
+        }
+        else
+        {
+            head = AddNodeAfter(end, x);
+        }
+    }
+    Print_List(head);
+
+    c = head;
+    do
+    {
+        if (abs(c->data) < abs(c->next->data))
+        {
+            c = DeleteNode(c, head, end);
+            c = c->next;
+        }
+        else if (abs(c->data) > abs(c->next->data))
+        {
+            c = c->next;
+            c = DeleteNode(c, head, end);
+            if (c == head) break;
+        }
+        else
+        {
+            c = c->next->next;
+        }
+    } while (c != head);
+
+    Print_List(head);
+
+
+    Free_Memory(head, end);
+}
